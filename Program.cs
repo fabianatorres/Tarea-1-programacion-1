9f300286f7cf1c577a0817a7a10a54a70d42606b
fabianatorres <92831080+fabianatorres@users.noreply.github.com>
2021-10-19 19:45:18 -0400

    Fabiana Torres. V-30.213.484. Tarea 1 PROG1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "6";
+            String b = "9";
+            String c = "4";
+
+            String aacb = a + a + c + b;
+            String bca = b + c + a;
+            String bcbc = b + c + b + c;
+
+            Console.WriteLine("aacb= " + aacb);
+            Console.WriteLine("bca= " + bca);
+            Console.WriteLine("bcbc= " + bcbc);
+
+        }
+    }
+}
